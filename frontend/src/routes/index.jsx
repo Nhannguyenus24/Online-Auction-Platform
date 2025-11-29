70e607fdc46d0ac1fc1e6a5a8cd8e8e55f7117da
Trần Tiến Lợi <trantienloi2404@gmail.com>
2025-11-29 21:32:49 +0700

    feat(frontend): restructure routing for outlet

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -5,43 +5,57 @@ import LoadingScreen from "../components/LoadingScreen";
 import AuthGuard from "../guards/AuthGuard";
 // ----------------------------------------------------------------------
 
-const Loadable = (Component) => (props) => (
-  <Suspense fallback={<LoadingScreen />}>
-    <Component {...props} />
-  </Suspense>
-);
-
+const Loadable = (Component) => (props) =>
+  (
+    <Suspense fallback={<LoadingScreen />}>
+      <Component {...props} />
+    </Suspense>
+  );
 
 export default function Router() {
   return useRoutes([
     // Main Routes
     {
-      path: "*",
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <HomePage /> },
+        { path: "category", element: <CategoryPage /> },
+        { path: "category/:parentCategory", element: <CategoryPage /> },
+        {
+          path: "category/:parentCategory/:childCategory",
+          element: <CategoryPage />,
+        },
+        { path: "cookie-policy", element: <CookiePolicyPage /> },
+        { path: "careers", element: <CareersPage /> },
+        { path: "help", element: <HelpCenterPage /> },
+        { path: "how-to-bid", element: <HowToBidPage /> },
+        { path: "seller-guide", element: <SellerGuidePage /> },
+        { path: "privacy-policy", element: <PrivacyPage /> },
+        { path: "terms-of-service", element: <TermOfServicePage /> },
+        { path: "about-us", element: <AboutUs /> },
+        { path: "maintenance", element: <Maintenance /> },
+        { path: "product/:id", element: <ProductDetailPage /> },
+        { path: "500", element: <Page500 /> },
+        { path: "404", element: <Page404 /> },
+      ],
+    },
+    {
+      path: "auth",
       children: [
         { path: "login", element: <Login /> },
         { path: "forgot-password", element: <ForgotPassword /> },
         { path: "register", element: <Register /> },
         { path: "reset-password", element: <ResetPassword /> },
-        { path: "about-us", element: <AboutUs /> },
-        { path: "maintenance", element: <Maintenance /> },
-        { path: "500", element: <Page500 /> },
-        { path: "404", element: <Page404 /> },
-        { path: "product/:id", element: <ProductDetailPage /> },
-        { path: "category", element: <MainLayout><CategoryPage /></MainLayout> },
-        { path: "cookie-policy", element: <MainLayout><CookiePolicyPage /></MainLayout> },
-        { path: "careers", element: <MainLayout><CareersPage /></MainLayout> },
-        { path: "help", element: <MainLayout><HelpCenterPage /></MainLayout> },
-        { path: "how-to-bid", element: <MainLayout><HowToBidPage /></MainLayout> },
-        { path: "seller-guide", element: <MainLayout><SellerGuidePage /></MainLayout> },
-        { path: "privacy-policy", element: <MainLayout><PrivacyPage /></MainLayout> },
-        { path: "terms-of-service", element: <MainLayout><TermOfServicePage /></MainLayout> },
-        { path: "", element: <MainLayout><HomePage /></MainLayout> },
-        { path: "*", element: <Navigate to="/404" replace /> },
       ],
     },
     {
       path: "admin",
-      element: <AuthGuard><AdminLayout /></AuthGuard>,
+      element: (
+        <AuthGuard>
+          <AdminLayout />
+        </AuthGuard>
+      ),
       children: [
         { path: "dashboard", element: <DashboardAdmin /> },
         { path: "users", element: <UserManagementPage /> },
@@ -74,55 +88,103 @@ export default function Router() {
         { path: "chat", element: <SellerChatPage /> },
         { path: "chat/:orderId", element: <SellerChatPage /> },
       ],
-    }
+    },
+    { path: "*", element: <Navigate to="/404" replace /> },
   ]);
 }
 
-
 // AUTHENTICATION
 const Login = Loadable(lazy(() => import("../pages/authentication/Login.jsx")));
-const ForgotPassword = Loadable(lazy(() => import("../pages/authentication/ForgotPassword.jsx")));
-const ResetPassword = Loadable(lazy(() => import("../pages/authentication/ResetPassword.jsx")));
-const Register = Loadable(lazy(() => import("../pages/authentication/Register.jsx")));
+const ForgotPassword = Loadable(
+  lazy(() => import("../pages/authentication/ForgotPassword.jsx"))
+);
+const ResetPassword = Loadable(
+  lazy(() => import("../pages/authentication/ResetPassword.jsx"))
+);
+const Register = Loadable(
+  lazy(() => import("../pages/authentication/Register.jsx"))
+);
 
 // MAINLAYOUT
 const AdminLayout = Loadable(lazy(() => import("../layouts/AdminLayout.jsx")));
 const MainLayout = Loadable(lazy(() => import("../layouts/MainLayout.jsx")));
-const BidderLayout = Loadable(lazy(() => import("../layouts/BidderLayout.jsx")));
-const SellerLayout = Loadable(lazy(() => import("../layouts/SellerLayout.jsx")));
+const BidderLayout = Loadable(
+  lazy(() => import("../layouts/BidderLayout.jsx"))
+);
+const SellerLayout = Loadable(
+  lazy(() => import("../layouts/SellerLayout.jsx"))
+);
 
 //ADMIN
-const DashboardAdmin = Loadable(lazy(() => import("../pages/admin/DashBoardPage.jsx")));
-const UserManagementPage = Loadable(lazy(() => import("../pages/admin/UserManagementPage.jsx")));
-const CategoryManagementPage = Loadable(lazy(() => import("../pages/admin/CategoryManagementPage.jsx")));
-const ProductManagementPage = Loadable(lazy(() => import("../pages/admin/ProductManagementPage.jsx")));
+const DashboardAdmin = Loadable(
+  lazy(() => import("../pages/admin/DashBoardPage.jsx"))
+);
+const UserManagementPage = Loadable(
+  lazy(() => import("../pages/admin/UserManagementPage.jsx"))
+);
+const CategoryManagementPage = Loadable(
+  lazy(() => import("../pages/admin/CategoryManagementPage.jsx"))
+);
+const ProductManagementPage = Loadable(
+  lazy(() => import("../pages/admin/ProductManagementPage.jsx"))
+);
 
 //SELLER
-const SellerHomePage = Loadable(lazy(() => import("../pages/seller/HomePage.jsx")));
-const SellerProfilePage = Loadable(lazy(() => import("../pages/seller/ProfilePage.jsx")));
-const SellerCreateAuctionPage = Loadable(lazy(() => import("../pages/seller/CreateAuctionPage.jsx")));
-const SellerOrderListPage = Loadable(lazy(() => import("../pages/seller/OrderListPage.jsx")));
-const SellerChatPage = Loadable(lazy(() => import("../pages/seller/ChatPage.jsx")));
+const SellerHomePage = Loadable(
+  lazy(() => import("../pages/seller/HomePage.jsx"))
+);
+const SellerProfilePage = Loadable(
+  lazy(() => import("../pages/seller/ProfilePage.jsx"))
+);
+const SellerCreateAuctionPage = Loadable(
+  lazy(() => import("../pages/seller/CreateAuctionPage.jsx"))
+);
+const SellerOrderListPage = Loadable(
+  lazy(() => import("../pages/seller/OrderListPage.jsx"))
+);
+const SellerChatPage = Loadable(
+  lazy(() => import("../pages/seller/ChatPage.jsx"))
+);
 
 //BIDDER
-const BidderHomePage = Loadable(lazy(() => import("../pages/bidder/HomePage.jsx")));
-const BidderProfilePage = Loadable(lazy(() => import("../pages/bidder/ProfilePage.jsx")));
-const BidderWatchListPage = Loadable(lazy(() => import("../pages/bidder/WatchListPage.jsx")));
-const BidderAuctionHistoryPage = Loadable(lazy(() => import("../pages/bidder/AuctionHistory.jsx")));
-const BidderCheckoutPage = Loadable(lazy(() => import("../pages/bidder/CheckoutPage.jsx")));
+const BidderHomePage = Loadable(
+  lazy(() => import("../pages/bidder/HomePage.jsx"))
+);
+const BidderProfilePage = Loadable(
+  lazy(() => import("../pages/bidder/ProfilePage.jsx"))
+);
+const BidderWatchListPage = Loadable(
+  lazy(() => import("../pages/bidder/WatchListPage.jsx"))
+);
+const BidderAuctionHistoryPage = Loadable(
+  lazy(() => import("../pages/bidder/AuctionHistory.jsx"))
+);
+const BidderCheckoutPage = Loadable(
+  lazy(() => import("../pages/bidder/CheckoutPage.jsx"))
+);
 
 // OTHERS
 const Page500 = Loadable(lazy(() => import("../pages/Page500")));
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
 const AboutUs = Loadable(lazy(() => import("../pages/AboutUs")));
 const Maintenance = Loadable(lazy(() => import("../pages/Maintenance")));
-const ProductDetailPage = Loadable(lazy(() => import("../pages/ProductDetailPage.jsx")));
+const ProductDetailPage = Loadable(
+  lazy(() => import("../pages/ProductDetailPage.jsx"))
+);
 const CategoryPage = Loadable(lazy(() => import("../pages/CategoryPage.jsx")));
 const CareersPage = Loadable(lazy(() => import("../pages/CareersPage.jsx")));
-const CookiePolicyPage = Loadable(lazy(() => import("../pages/CookiePolicyPage.jsx")));
-const HelpCenterPage = Loadable(lazy(() => import("../pages/HelpCenterPage.jsx")));
+const CookiePolicyPage = Loadable(
+  lazy(() => import("../pages/CookiePolicyPage.jsx"))
+);
+const HelpCenterPage = Loadable(
+  lazy(() => import("../pages/HelpCenterPage.jsx"))
+);
 const HowToBidPage = Loadable(lazy(() => import("../pages/HowToBidPage.jsx")));
-const SellerGuidePage = Loadable(lazy(() => import("../pages/SellerGuidePage.jsx")));
+const SellerGuidePage = Loadable(
+  lazy(() => import("../pages/SellerGuidePage.jsx"))
+);
 const PrivacyPage = Loadable(lazy(() => import("../pages/PrivacyPage.jsx")));
-const TermOfServicePage = Loadable(lazy(() => import("../pages/TermOfServicePage.jsx")));
+const TermOfServicePage = Loadable(
+  lazy(() => import("../pages/TermOfServicePage.jsx"))
+);
 const HomePage = Loadable(lazy(() => import("../pages/HomePage.jsx")));
